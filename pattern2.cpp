a961efe58934a425b9cd9b9180d8d2a2aa8cbc77
Tanisha Jain <itanishajain@gmail.com>
2024-09-10 23:59:39 +0530

    patterns

--- pattern2.cpp
+++ pattern2.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 int main(){
     int n ;
-    cout << "Enter n : " ;
+    cout << "Enter n : ";
     cin >> n ;
 
     int i = 1;
     while(i<=n){
         int j = 1;
-        while (j<=n)
-        {
-            cout << i;
-            j += 1;
+        while(j<=n){
+            char ch = 'A' + i - 1;
+            cout << ch << "\t";
+            j++;
         }
         cout << endl;
-        i += 1;
+        i++;
     }
     return 0;
 }
