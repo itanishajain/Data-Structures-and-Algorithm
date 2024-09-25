340ac8b2fab42c58d21e2c145e8c1b120338401a
Tanisha Jain <itanishajain@gmail.com>
2024-09-25 19:56:15 +0530

    operators

---
+++ operator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 1, b = 2;
+    if (a-- > 0 || ++b > 2)
+    {
+        cout << "Stage1- inside-if" ;
+    }
+    else{
+        cout << "Stage2- inside-else" ;
+    }
+    return 0;
+}
