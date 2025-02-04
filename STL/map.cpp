4e34b3e9c44bba63485f20613b5c0eacf92c982e
Tanisha Jain <itanishajain@gmail.com>
2025-02-04 20:04:57 +0530

    map in stl

---
+++ STL/map.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <map> // Correct header for std::map
+using namespace std;
+
+int main() {
+    map<int,string> m; // std::map should be recognized now
+
+    m[1] = "abc";
+    m[5] = "cdc";
+    m[3] = "efg";
+
+    m.insert({{7, "abc"}, {8, "cdc"}, {6, "efg"}});
+
+    // Use `const auto&` to avoid copying elements
+    cout << "Before erase: " << endl;
+    for (const auto& i : m) {
+        cout << i.first <<" " << i.second << endl;
+    }
+    cout << "5 element: " << m.count(5) << endl;
+
+    cout << "After erase: " << endl;
+    m.erase(5);
+    for (const auto& i : m) {
+        cout << i.first <<" " << i.second << endl;
+    }
+    return 0;
+}
